337e56f77d3c22d9f2eeec20c46aafd49532b699
Divashka <dianakuzmina325@gmail.com>
2023-12-15 20:50:09 +0300

    Выделила компонент списка квестов

---
+++ src/components/quest-cards/quest-cards.tsx
@@ -0,0 +1,11 @@
+import QuestCard from '../quest-card/quest-card';
+
+function QuestCards(): JSX.Element {
+  return (
+    <div className="cards-grid">
+      <QuestCard></QuestCard>
+    </div>
+  );
+}
+
+export default QuestCards;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,6 @@
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
+import QuestCards from '../../components/quest-cards/quest-cards';
 
 function MainPage(): JSX.Element {
   return (
@@ -96,38 +97,7 @@ function MainPage(): JSX.Element {
             </form>
           </div>
           <h2 className="title visually-hidden">Выберите квест</h2>
-          <div className="cards-grid">
-            <div className="quest-card">
-              <div className="quest-card__img">
-                <picture>
-                  <source type="image/webp"
-                    srcSet="img/content/crypt/crypt-size-s.webp, img/content/crypt/crypt-size-s@2x.webp 2x"
-                  >
-                  </source>
-                  <img
-                    src="img/content/crypt/crypt-size-s.jpg" srcSet="img/content/crypt/crypt-size-s@2x.jpg 2x" width="344"
-                    height="232" alt="Мужчина в клетке в подземелье."
-                  />
-                </picture>
-              </div>
-              <div className="quest-card__content">
-                <div className="quest-card__info-wrapper"><a className="quest-card__link" href="quest.html">Склеп</a>
-                </div>
-                <ul className="tags quest-card__tags">
-                  <li className="tags__item">
-                    <svg width="11" height="14" aria-hidden="true">
-                      <use xlinkHref="#icon-person"></use>
-                    </svg>2&ndash;5&nbsp;чел
-                  </li>
-                  <li className="tags__item">
-                    <svg width="14" height="14" aria-hidden="true">
-                      <use xlinkHref="#icon-level"></use>
-                    </svg>Сложный
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <QuestCards></QuestCards>
         </div>
       </main>
 
