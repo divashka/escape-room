e8e6ec02496405772f920608e2e084e1b9a059e4
Divashka <dianakuzmina325@gmail.com>
2023-12-16 14:32:57 +0300

    Фильтрация квестов на главной странице

--- src/const/const.ts
+++ src/const/const.ts
@@ -26,3 +26,48 @@ export enum SliceNameSpace {
   Reservation = 'RESERVATION',
   User = 'USER'
 }
+
+export const FilterSubjectItems = [
+  {
+    label: 'Приключения',
+    type: 'adventure',
+    icon: 'adventure'
+  },
+  {
+    label: 'Ужасы',
+    type: 'horror',
+    icon: 'horror'
+  },
+  {
+    label: 'Мистика',
+    type: 'mystic',
+    icon: 'mystic'
+  },
+  {
+    label: 'Детектив',
+    type: 'detective',
+    icon: 'detective'
+  },
+  {
+    label: 'Sci-fi',
+    type: 'sci-fi',
+    icon: 'sci-fi'
+  },
+];
+
+export const FilterLevelLabels = {
+  easy: 'Лёгкий',
+  medium: 'Средний',
+  hard: 'Сложный',
+};
+
+export const FilterLevelDefault = {
+  label: 'Любой',
+  type: 'any'
+};
+
+export const FilterSubjectDefault = {
+  label: 'Все квесты',
+  type: 'all',
+  icon: 'all-quests'
+};
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,12 +1,39 @@
+import { useState } from 'react';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import QuestCards from '../../components/quest-cards/quest-cards';
 import { useAppSelector } from '../../hooks';
 import { getQuests } from '../../store/quest-slice/selectors';
+import { FilterSubjectItems, FilterLevelLabels, FilterSubjectDefault, FilterLevelDefault } from '../../const/const';
+import { TypeQuest, LevelQuest } from '../../types/types';
 
 function MainPage(): JSX.Element {
 
+  const [activeFilterSubjectItem, setActiveFilterSubjectItem] = useState<TypeQuest | null>(null);
+
+  const [activeFilterLevelItem, setActiveFilterLevelItem] = useState<LevelQuest | null>(null);
+
   const quests = useAppSelector(getQuests);
+
+  let filteredSubjectQuests = quests.filter((quest) => quest.type === activeFilterSubjectItem);
+
+  if (activeFilterSubjectItem === null) {
+    filteredSubjectQuests = quests;
+  }
+
+  let filteredLevelQuests = filteredSubjectQuests.filter((quest) => quest.level === activeFilterLevelItem);
+
+  if (activeFilterLevelItem === null) {
+    filteredLevelQuests = filteredSubjectQuests;
+  }
+
+  function handleFilterSubjectChange(type: TypeQuest | null) {
+    setActiveFilterSubjectItem(type);
+  }
+
+  function handleFilterLevelChange(type: LevelQuest | null) {
+    setActiveFilterLevelItem(type);
+  }
 
   return (
     <div className="wrapper">
@@ -25,84 +52,77 @@ function MainPage(): JSX.Element {
                 <legend className="visually-hidden">Тематика</legend>
                 <ul className="filter__list">
                   <li className="filter__item">
-                    <input type="radio" name="type" id="all" ></input>
-                    <label className="filter__label" htmlFor="all">
+                    <input
+                      type="radio"
+                      name="type"
+                      id={FilterSubjectDefault.type}
+                      onChange={() => handleFilterSubjectChange(null)}
+                      checked={activeFilterSubjectItem === null}
+                    />
+                    <label className="filter__label" htmlFor={FilterSubjectDefault.type}>
                       <svg className="filter__icon" width="26" height="30" aria-hidden="true">
-                        <use xlinkHref="#icon-all-quests"></use>
-                      </svg><span className="filter__label-text">Все квесты</span>
+                        <use xlinkHref={`#icon-${FilterSubjectDefault.icon}`}></use>
+                      </svg><span className="filter__label-text">{FilterSubjectDefault.label}</span>
                     </label>
                   </li>
-                  <li className="filter__item">
-                    <input type="radio" name="type" id="adventure"></input>
-                    <label className="filter__label" htmlFor="adventure" >
-                      <svg className="filter__icon" width="36" height="30" aria-hidden="true">
-                        <use xlinkHref="#icon-adventure"></use>
-                      </svg><span className="filter__label-text">Приключения</span>
-                    </label>
-                  </li>
-                  <li className="filter__item">
-                    <input type="radio" name="type" id="horror"></input>
-                    <label className="filter__label" htmlFor="horror" >
-                      <svg className="filter__icon" width="30" height="30" aria-hidden="true">
-                        <use xlinkHref="#icon-horror"></use>
-                      </svg><span className="filter__label-text">Ужасы</span>
-                    </label>
-                  </li>
-                  <li className="filter__item">
-                    <input type="radio" name="type" id="mystic"></input>
-                    <label className="filter__label" htmlFor="mystic">
-                      <svg className="filter__icon" width="30" height="30" aria-hidden="true">
-                        <use xlinkHref="#icon-mystic"></use>
-                      </svg><span className="filter__label-text">Мистика</span>
-                    </label>
-                  </li>
-                  <li className="filter__item">
-                    <input type="radio" name="type" id="detective"></input>
-                    <label className="filter__label" htmlFor="detective" >
-                      <svg className="filter__icon" width="40" height="30" aria-hidden="true">
-                        <use xlinkHref="#icon-detective"></use>
-                      </svg><span className="filter__label-text">Детектив</span>
-                    </label>
-                  </li>
-                  <li className="filter__item">
-                    <input type="radio" name="type" id="sciFi"></input>
-                    <label className="filter__label" htmlFor="sciFi" >
-                      <svg className="filter__icon" width="28" height="30" aria-hidden="true">
-                        <use xlinkHref="#icon-sci-fi"></use>
-                      </svg><span className="filter__label-text">Sci-fi</span>
-                    </label>
-                  </li>
+                  {FilterSubjectItems.map(({ type, label, icon }) => (
+                    <li
+                      key={type}
+                      tabIndex={0}
+                      className="filter__item"
+                    >
+                      <input
+                        type="radio"
+                        name="type"
+                        id={type}
+                        onChange={() => handleFilterSubjectChange(type as TypeQuest)}
+                        checked={activeFilterSubjectItem === type}
+                      >
+                      </input>
+                      <label className="filter__label" htmlFor={type}>
+                        <svg className="filter__icon" width="26" height="30" aria-hidden="true">
+                          <use xlinkHref={`#icon-${icon}`}></use>
+                        </svg><span className="filter__label-text">{label}</span>
+                      </label>
+                    </li>
+                  ))}
                 </ul>
               </fieldset>
               <fieldset className="filter__section">
                 <legend className="visually-hidden">Сложность</legend>
                 <ul className="filter__list">
-                  <li className="filter__item">
-                    <input type="radio" name="level" id="any" ></input>
-                    <label className="filter__label" htmlFor="any"><span className="filter__label-text">Любой</span>
+                  <li key={FilterLevelDefault.type} className="filter__item">
+                    <input
+                      type="radio"
+                      name="level"
+                      id={FilterLevelDefault.type}
+                      checked={activeFilterLevelItem === null}
+                      onChange={() => handleFilterLevelChange(null)}
+                    >
+                    </input>
+                    <label className="filter__label" htmlFor={FilterLevelDefault.type}><span className="filter__label-text">{FilterLevelDefault.label}</span>
                     </label>
                   </li>
-                  <li className="filter__item">
-                    <input type="radio" name="level" id="easy"></input>
-                    <label className="filter__label" htmlFor="easy"><span className="filter__label-text">Лёгкий</span>
-                    </label>
-                  </li>
-                  <li className="filter__item">
-                    <input type="radio" name="level" id="middle"></input>
-                    <label className="filter__label" htmlFor="middle"><span className="filter__label-text">Средний</span>
-                    </label>
-                  </li>
-                  <li className="filter__item">
-                    <input type="radio" name="level" id="hard"></input>
-                    <label className="filter__label" htmlFor="hard"><span className="filter__label-text">Сложный</span>
-                    </label>
-                  </li>
+                  {Object.entries(FilterLevelLabels).map(([type, label]) => (
+                    <li key={type} className="filter__item">
+                      <input
+                        type="radio"
+                        name="level"
+                        id={type}
+                        checked={activeFilterLevelItem === type}
+                        onChange={() => handleFilterLevelChange(type as LevelQuest)}
+                      >
+                      </input>
+                      <label className="filter__label" htmlFor={type}><span className="filter__label-text">{label}</span>
+                      </label>
+                    </li>
+                  ))}
                 </ul>
               </fieldset>
             </form>
           </div>
           <h2 className="title visually-hidden">Выберите квест</h2>
-          <QuestCards quests={quests}></QuestCards>
+          <QuestCards quests={filteredLevelQuests}></QuestCards>
         </div>
       </main>
 
