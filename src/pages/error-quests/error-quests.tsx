de5799191eb196eebd8d55b1def64b2715e6eb1a
Divashka <dianakuzmina325@gmail.com>
2023-12-21 18:17:47 +0300

    Компонент ошибка загрузки квестов

---
+++ src/pages/error-quests/error-quests.tsx
@@ -0,0 +1,32 @@
+import { fetchQuestsAction } from '../../store/api-actions';
+import { useAppDispatch } from '../../hooks';
+import './error-quests.css';
+
+function ErrorQuestsPage(): JSX.Element {
+
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="wrapper">
+
+      <main className="page-content">
+        <div className="container">
+          <div className="error__wrapper">
+            <h1 className="title title--size-m page-content__title">Произошла ошибка загрузки данных</h1>
+            <button
+              className="error__button button"
+              onClick={() => {
+                dispatch(fetchQuestsAction());
+              }}
+            >
+              Попробовать ещё раз
+            </button>
+          </div>
+        </div>
+      </main>
+
+    </div>
+  );
+}
+
+export default ErrorQuestsPage;
