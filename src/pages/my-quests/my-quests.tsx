7f1f973a53f2142b54c02b45ff7e360118d4bb4e
Divashka <dianakuzmina325@gmail.com>
2023-12-21 18:19:36 +0300

    Работа над страницей мои бронирования

--- src/pages/my-quests/my-quests.tsx
+++ src/pages/my-quests/my-quests.tsx
@@ -3,6 +3,7 @@ import Header from '../../components/header/header';
 import { useAppSelector } from '../../hooks';
 import { getBookingQuests } from '../../store/reservation-slice/selectors';
 import NotBookingQuests from '../../components/not-booking-quests/not-booking-quests';
+import QuestBookingCards from '../../components/quest-booking-cards/quest-booking-cards';
 
 function MyQuestsPage(): JSX.Element {
 
@@ -30,46 +31,7 @@ function MyQuestsPage(): JSX.Element {
           </div>
           {
             bookingQuests.length === 0 ? <NotBookingQuests></NotBookingQuests> :
-              <div className="cards-grid">
-                <div className="quest-card">
-                  <div className="quest-card__img">
-                    <picture>
-                      <source type="image/webp"
-                        srcSet="img/content/maniac/maniac-size-s.webp, img/content/maniac/maniac-size-s@2x.webp 2x"
-                      >
-
-                      </source>
-                      <img
-                        src="img/content/maniac/maniac-size-s.jpg" srcSet="img/content/maniac/maniac-size-s@2x.jpg 2x"
-                        width="344" height="232" alt="Мужчина в маске в тёмном переходе."
-                      />
-                    </picture>
-                  </div>
-                  <div className="quest-card__content">
-                    <div className="quest-card__info-wrapper"><a className="quest-card__link" href="quest.html">Маньяк</a>
-                      <span
-                        className="quest-card__info"
-                      >
-                        [сегодня,&nbsp;17:00. наб. реки Карповки&nbsp;5, лит&nbsp;П<br></br>м.
-                        Петроградская]
-                      </span>
-                    </div>
-                    <ul className="tags quest-card__tags">
-                      <li className="tags__item">
-                        <svg width="11" height="14" aria-hidden="true">
-                          <use xlinkHref="#icon-person"></use>
-                        </svg>6&nbsp;чел
-                      </li>
-                      <li className="tags__item">
-                        <svg width="14" height="14" aria-hidden="true">
-                          <use xlinkHref="#icon-level"></use>
-                        </svg>Средний
-                      </li>
-                    </ul>
-                    <button className="btn btn--accent btn--secondary quest-card__btn" type="button">Отменить</button>
-                  </div>
-                </div>
-              </div>
+              <QuestBookingCards quests={bookingQuests}></QuestBookingCards>
           }
         </div>
       </main>
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,21 @@
 import { AuthorizationStatus } from '../const/const';
 import { store } from '../store/store';
 
+export type BookingDate = 'today' | 'tomorrow';
+
+export type State = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export type LevelQuest = 'easy' | 'medium' | 'hard';
+
+export type TypeQuest = 'adventures' | 'horror' | 'mystic' | 'detective' | 'sci-fi';
+
+export type Location = {
+  address: string;
+  coords: [number, number];
+}
+
 export type Quest = {
   id: string;
   title: string;
@@ -17,15 +32,45 @@ export type QuestFull = Quest & {
   coverImgWebp: string;
 }
 
-export type LevelQuest = 'easy' | 'medium' | 'hard';
+export type infoBookingQuest = {
+  id: string;
+  location: {
+    address: string;
+    coords: [number, number];
+  };
+  slots: BookingSlots;
+}
 
-export type TypeQuest = 'adventures' | 'horror' | 'mystic' | 'detective' | 'sci-fi';
+export type BookingSlots = {
+  today: [{
+    time: string;
+    isAvailable: boolean;
+  }];
+  tomorrow: [{
+    time: string;
+    isAvailable: boolean;
+  }];
+}
+
+export type BookingQuest = {
+  date: BookingDate;
+  time: string;
+  contactPerson: string;
+  phone: string;
+  withChildren: boolean;
+  peopleCount: number;
+  id: string;
+  location: Location;
+  quest: Quest;
+}
 
 export type QuestSlice = {
   quests: Quest[];
   oneQuest: QuestFull | null;
   isOneQuestLoading: boolean;
   hasErrorOneQuest: boolean;
+  hasErrorQuests: boolean;
+  isQuestsLoading: boolean;
 };
 
 export type UserSlice = {
@@ -41,62 +86,22 @@ export type BookingSlice = {
   infoBookingQuest: infoBookingQuest[];
 };
 
-export type infoBookingQuest = {
-  id: string;
-  location: {
-    address: string;
-    coords: [number, number];
-  };
-  slots: {
-    today: [
-      {
-        time: string;
-        isAvailable: boolean;
-      }
-    ];
-    tomorrow: [
-      {
-        time: string;
-        isAvailable: boolean;
-      }
-    ];
-  };
-}
-
-export type BookingQuest = {
-  date: BookingDate;
-  time: string;
-  contactPerson: string;
-  phone: string;
-  withChildren: boolean;
-  peopleCount: number;
-  id: string;
-  location: Location;
-  quest: {
-    id: string;
-    title: string;
-    previewImg: string;
-    previewImgWebp: string;
-    level: LevelQuest;
-    type: TypeQuest;
-    peopleMinMax: [number, number];
-  };
-}
-
-export type BookingDate = 'today' | 'tomorrow';
-
-export type State = ReturnType<typeof store.getState>;
-
-export type AppDispatch = typeof store.dispatch;
-
-export type Location = {
-  address: string;
-  coords: [number, number];
-}
-
 export type AuthData = {
   email: string;
   token: string;
+}
+
+export type BookQuestData = {
+  id: string;
+  data: {
+    date: BookingDate;
+    time: string;
+    contactPerson: string;
+    phone: string;
+    withChildren: boolean;
+    peopleCount: number;
+    placeId: string;
+  };
 }
 
 export type UserData = {
