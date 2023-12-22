51d4b69232be54dff65e63906b055c1e030d0c70
Divashka <dianakuzmina325@gmail.com>
2023-12-22 12:22:37 +0300

    Компонент ошибка отправки данных

---
+++ src/components/error-sending/error-sending.tsx
@@ -0,0 +1,9 @@
+import './error-sending.css';
+
+function ErrorSending(): JSX.Element {
+  return (
+    <div className="error-submit">Произошла ошибка отправки данных. Попробуйте ещё раз</div>
+  );
+}
+
+export default ErrorSending;
